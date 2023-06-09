01df7e9e862e954b462240c8139925b614a611e0
Bruno Meireles <brunomeireles23@gmail.com>
2023-06-09 09:16:55 -0300

    commit inicial

---
+++ packages/docs/.storybook/main.js
@@ -0,0 +1,27 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/pages/**/*stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  viteFinal:(config, {configType}) => {
+    if(configType === 'PRODUCTION'){
+      config.base = '/Projeto-Estudo-Design-System/'
+    }
+    return config
+  }
+};
+export default config;
